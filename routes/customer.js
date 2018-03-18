a69df186f3ecc1e0eda14e9c21088d49a782b3c9
pushpendraraj <rajput.pushpendra61@gmail.com>
2018-03-19 01:45:36 +0530

    mail

--- routes/customer.js
+++ routes/customer.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var Customer = require('../models/Customer');
+var transporter = require('../mailer');
 
 router.get('/list-customers',function(req, res, next){
     Customer.getCustomers(function(err, result){
         if(err) return next(err)
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Fred Foo 👻" <rajput.pushpendra62@gmail.com>', // sender address
+            to: 'rajput.pushpendra62@gmail.com, rajput.pushpendra61@gmail.com', // list of receivers
+            subject: 'Hello ✔', // Subject line
+            text: 'Hello world?', // plain text body
+            html: '<b>Hello world?</b>', // html body
+            attachments : [{filename:'text1.txt',content:'Hello World!'}]
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
         res.render('customer/index.ejs',{'data':result})
     })
 })
