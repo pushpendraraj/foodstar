188dd213c6e2dd30c3ed0f638dab84e4f3efdf00
Pushpendra Rajput <prerna.g@dimensionindia.com>
2018-03-21 20:09:56 +0530

    pagination

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -6,8 +6,34 @@ router.get('/list-restaurants', function(req, res, next){
     Restaurant.getResturants(function(err, result){
         if(err) return next(err);
         sess = req.session;
+        var totalStudents = 80,
+		pageSize = 8,
+		pageCount = 80/8,
+        currentPage = 1,
+        studentsArrays = [], 
+		studentsList = [];
+		
+		//split list into groups
+	while (result.length > 0) {
+	    studentsArrays.push(result.splice(0, pageSize));
+	}
+
+	//set current page if specifed as get variable (eg: /?page=2)
+	if (typeof req.query.page !== 'undefined') {
+		currentPage = +req.query.page;
+	}
+
+	//show list of students from group
+	studentsList = studentsArrays[+currentPage - 1];
+        
         if(sess.isLoggedIn){
-            res.render('restaurant/index', {'data':result});
+            res.render('restaurant/index',  {
+                data: studentsList,
+                pageSize: pageSize,
+                totalStudents: totalStudents,
+                pageCount: pageCount,
+                currentPage: currentPage
+            });
         }else{
             res.redirect('/');
         }
