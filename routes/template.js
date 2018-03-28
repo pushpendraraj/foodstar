7f871d2cd1ac3005a9f596745d42d7abb731baf1
Pushpendra Rajput <prerna.g@dimensionindia.com>
2018-03-28 20:06:42 +0530

    template view

--- routes/template.js
+++ routes/template.js
@@ -2,11 +2,20 @@ var express = require('express');
 var router = express.Router();
 var EmailTemplate = require('../models/EmailTemplate');
 
-router.all('/list-templates', function(req, res, next){
+router.get('/list-templates', function(req, res, next){
     EmailTemplate.getTemplate({template_status : 'Active'}, '*', function(err, templates){
         if(err) return next(err)
         res.render('template/index.ejs',{templates:templates});
     })   
+})
+
+router.get('/view-template/:tmeplateId', function(req, res, next){
+    console.log(req.params);
+    let templateId = req.params.templateId;
+    EmailTemplate.getTemplate({template_id : templateId}, '*', function(err, templates){
+        if(err) return next(err)
+        res.render('template/view.ejs',{templates:templates});
+    }) 
 })
 
 module.exports = router;
