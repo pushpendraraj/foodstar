c2709eef23c04877cda82f6d56bc3fd2e8c059a2
pushpendraraj <rajput.pushpendra61@gmail.com>
2018-04-30 01:37:52 +0530

    chagnes for login

--- config.js
+++ config.js
@@ -11,6 +11,12 @@ configuration = {
         invalidLogin:'<strong>Sorry</strong> ! Invalid email or password.',
         loginSuccess:'<strong>Thnak You</strong> ! User login successfully.',
         serverError:'<strong>Sorry</strong> ! Something went wrong, please try again after some time.',
+        emailExist: `<strong>Sorry</strong> ! Email already register with us, don't know your password? go for forgot password.`,
+        invalidEmail:`Invalid email address.`,
+        invalidMobile:`Invalid mobile.`,
+        requiredFullname:`Full name is required.`,
+        requiredPassword:`password is required.`,
+        invalidFieldValues:`You have enter invalid field values, please try again with valid data.`,
     },
     'enableMessage':true,
 }
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ var Customer = require('../models/Customer');
 var passwordHash = require('password-hash');
 var transporter = require('../mailer');
 var generator = require('generate-password');
+var smsGateway = require('sms-gateway-nodejs')('rajput.pushpendra62@gmail.com', '123456');
 
 router.get('/otp', function(req, res, next){
     let result = '8130606975';
@@ -29,73 +30,91 @@ router.get('/list-templates', function(req, res, next){
 
 router.post('/register-user', function(req, res, next){
     if(req.body){
-        let userData = {
-            'customer_role_id':3,
-            'customer_name':req.body.fullName,
-            'email':req.body.email,
-            'contact_no':req.body.contactNo,
-            'gender':null,
-            'password':(req.body.password =='')?'':passwordHash.generate(req.body.password),
-            'contact_address':'',
-            'dob':'',
-            'customer_status':1,
-            'is_contact_verified':0,
-            'customer_added_date':new Date(),
-            'customer_modified_date':new Date(),
-        };
+        req.assert('email', configuration.notificationMessages.invalidEmail).isEmail();
+        req.assert('contactNo', configuration.notificationMessages.invalidMobile).isNumeric();
+        req.assert('fullName', configuration.notificationMessages.requiredFullname).notEmpty();
+        req.assert('password', configuration.notificationMessages.requiredPassword).notEmpty();
+        let errors = req.validationErrors();
+        if(errors){
+            res.send({register:false,message:configuration.notificationMessages.invalidFieldValues});
+        }else{
+            Customer.isCustomerExist('email= "'+req.body.email+'" AND customer_status = 1', function(err, result){
+                if(err) return res.send({register:false, message:configuration.notificationMessages.serverError});
 
-        Customer.addCustomer(userData, function(err, result){
-            if(err) return next(err);
-
-            let promise1 = new Promise((resolve, reject)=>{
-                EmailTemplate.getTemplate({template_key:'customer_register'}, '*', function(err, result){
-                    if(err) return next(err);
-                    let tplData = result[0];
-
-                    //setup email data with unicode symbols
-                    let emailBody = tplData.email_body;
-                    let customerName = userData.customer_name;
-                    let tomail = userData.email;
-                    // emailBody = emailBody.replace('{email}', userData.email);
-                    // emailBody = emailBody.replace('{customer_name}', customerName);
-                    // emailBody = emailBody.replace('{password}', req.body.password);
-                    let from_email = tplData.from_email;
-                    let from_name = tplData.from_name;
-                    let email_subject = tplData.email_subject;
-                
-                    let mailOptions = {
-                        from: `${from_name} < ${from_email} >`, // sender address
-                        to: `${tomail}`, // list of receivers
-                        subject: `${email_subject} : ${configuration.projectName}`, // Subject line
-                        html: emailBody, // html body
-                    };
-    
-                    // send mail with defined transport object
-                    transporter.sendMail(mailOptions, (error, info) => {
-                        if (error) return next(error);
-                        resolve('Email send successfully.');
-                    });
-                });    
-            });
-
-            let promise2 = new Promise((resolve, reject)=>{
-                if(configuration.notificationMessages.enableMessage){
-                    resolve('Message send successfully');
+                if(result[0].count >0 ){
+                    res.send({register:false,message:configuration.notificationMessages.emailExist});
                 }else{
-                    resolve('Message api is disable');
-                }  
-            });
+                    let userData = {
+                        'customer_role_id':3,
+                        'customer_name':req.body.fullName,
+                        'email':req.body.email,
+                        'contact_no':req.body.contactNo,
+                        'gender':null,
+                        'password':(req.body.password =='')?'':passwordHash.generate(req.body.password),
+                        'contact_address':'',
+                        'dob':'',
+                        'customer_status':1,
+                        'is_contact_verified':0,
+                        'customer_added_date':new Date(),
+                        'customer_modified_date':new Date(),
+                    };
             
-            Promise.all([promise1, promise2])
-            .then((values)=>{
-                let message = configuration.notificationMessages.userResiterSuccessNotify;
-                return res.send({result:result, userData:userData, message:message});
-            })
-            .catch(() => { 
-                let message = configuration.notificationMessages.userResiterSuccess;
-                return res.send({result:result, userData:userData, message:message});
+                    Customer.addCustomer(userData, function(err, result){
+                        if(err) return res.send({register:false, message:configuration.notificationMessages.serverError});
+                        
+                        let promise1 = new Promise((resolve, reject)=>{
+                            EmailTemplate.getTemplate({template_key:'customer_register'}, '*', function(err, result){
+                                if(err) return res.send({register:false, message:configuration.notificationMessages.serverError});
+                                
+                                let tplData = result[0];            
+                                //setup email data with unicode symbols
+                                let emailBody = tplData.email_body;
+                                let customerName = userData.customer_name;
+                                let tomail = userData.email;
+                                // emailBody = emailBody.replace('{email}', userData.email);
+                                // emailBody = emailBody.replace('{customer_name}', customerName);
+                                // emailBody = emailBody.replace('{password}', req.body.password);
+                                let from_email = tplData.from_email;
+                                let from_name = tplData.from_name;
+                                let email_subject = tplData.email_subject;
+                            
+                                let mailOptions = {
+                                    from: `${from_name} < ${from_email} >`, // sender address
+                                    to: `${tomail}`, // list of receivers
+                                    subject: `${email_subject} : ${configuration.projectName}`, // Subject line
+                                    html: emailBody, // html body
+                                };
+                
+                                // send mail with defined transport object
+                                transporter.sendMail(mailOptions, (error, info) => {
+                                    if(error) return res.send({register:false, message:configuration.notificationMessages.serverError});
+
+                                    resolve('Email send successfully.');
+                                });
+                            });    
+                        });
+            
+                        let promise2 = new Promise((resolve, reject)=>{
+                            if(configuration.notificationMessages.enableMessage){
+                                resolve('Message send successfully');
+                            }else{
+                                resolve('Message api is disable');
+                            }  
+                        });
+                        
+                        Promise.all([promise1, promise2])
+                        .then((values)=>{
+                            let message = configuration.notificationMessages.userResiterSuccessNotify;
+                            return res.send({register:true, result:result, userData:userData, message:message});
+                        })
+                        .catch(() => { 
+                            let message = configuration.notificationMessages.userResiterSuccess;
+                            return res.send({register:true, result:result, userData:userData, message:message});
+                        });
+                    })
+                }
             });
-        })
+        }
     }
 })
 
@@ -105,13 +124,24 @@ router.post('/login-user', function(req, res, next){
         'password':req.body.password
     };
     req.assert('email', 'Invalid email address.').isEmail();
-    req.assert('password', 'password is required.').isEmpty();
+    req.assert('password', 'password is required.').notEmpty();
     let errors = req.validationErrors();
     if(errors){
         res.send({login:false, message:configuration.notificationMessages.invalidLogin});
     } else {
+        // smsGateway.message.sendMessageToNumber('87419', '+918130606975', 'Hello world :)')
+        // .then((response) => {
+        //     console.log(response)
+        //     // do something with response
+        // })
+        // .catch((error) => {
+        //     // handle error
+        //     console.log(error)
+        // });
+
         Customer.getCustomerDetails('*', 'email= "'+userData.email+'" AND customer_status = 1', function(err, result){
-            if(err) return next(err)
+            if(err) return res.send({login:false, message:configuration.notificationMessages.serverError});
+
             if(passwordHash.verify(userData.password, result[0].password)){   
                 res.send({login:true, result:result[0], message:configuration.notificationMessages.loginSuccess})
             }else{
@@ -128,10 +158,12 @@ router.post('/forgot-password', function(req, res, next){
         res.send({isSuccess:false, msg:errors[0].msg});
     }else{
         Customer.isCustomerExist('email= "'+req.body.email+'" AND customer_status = 1', function(err, result){
-            if(err) return next(err)
+            if(err) return res.send({isSuccess:false, msg:configuration.notificationMessages.serverError});
+
             if(result[0].count > 0){
                 EmailTemplate.getTemplate({template_key:'forgot_password'},'*',function(err, result){
-                    if(err) return next(err)
+                    if(err) return res.send({isSuccess:false, msg:configuration.notificationMessages.serverError});
+
                     let newPass = generator.generate({
                         length: 6,
                         numbers: false
@@ -140,9 +172,11 @@ router.post('/forgot-password', function(req, res, next){
                     let emailBody = result[0].email_body;
 
                     Customer.updateCustomer({'password':encodePass},{'email':req.body.email}, function(err, result){
-                        if(err) return next(err)
+                        if(err) return res.send({isSuccess:false, msg:configuration.notificationMessages.serverError});
+
                         Customer.getCustomerDetails('customer_name','email="'+req.body.email+'"', function(err, result){
-                        if(err) return next(err)
+                            if(err) return res.send({isSuccess:false, msg:configuration.notificationMessages.serverError});
+
                             let customerName = result[0].customer_name;
                             //setup email data with unicode symbols
                             emailBody = emailBody.replace('{email}', req.body.email);
@@ -158,7 +192,8 @@ router.post('/forgot-password', function(req, res, next){
             
                             // send mail with defined transport object
                             transporter.sendMail(mailOptions, (error, info) => {
-                                if (error) return next(error)
+                                if(error) return res.send({isSuccess:false, msg:configuration.notificationMessages.serverError});
+                                
                                 res.send({isSuccess:true, msg:'Password reset successfully, a new password has been send to your register email.'});
                             });
                        });
